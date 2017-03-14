fc04ba720168ee63781b2345d7a2034e3587b5fc
kimmihyun <kim20170@naver.com>
2017-03-14 10:19:42 +0900

    557p - 총알발사

---
+++ Assets/02.Scripts/FireCannon.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireCannon : MonoBehaviour {
+
+    private GameObject cannon = null;
+    private AudioClip fireSfx = null;
+    
+    private Transform firePos = null;
+    private AudioSource sfx = null;
+
+    private void Awake()
+    {
+        cannon = (GameObject)Resources.Load("Cannon");
+        fireSfx = (AudioClip)Resources.Load("CannonFire");
+
+        sfx = GetComponent<AudioSource>();
+        firePos = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            Fire();
+        }
+	}
+
+    void Fire() {
+        sfx.PlayOneShot(fireSfx, 1.0f);
+        Instantiate(cannon, firePos.position, firePos.rotation);
+    }
+}
